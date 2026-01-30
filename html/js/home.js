a9f36442c32e14cd5e74a9be62436d30679667ba
the-sisyphean <sg.chaithanya@gmail.com>
2026-01-31 00:09:08 +0530

    updates

--- html/js/home.js
+++ html/js/home.js
@@ -14,6 +14,48 @@ async function loadEvents() {
   });
 }
 
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+const layout = document.querySelector(".main-layout");
+
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  layout.classList.toggle("sidebar-open");
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !sidebar.contains(e.target) &&
+    !menuToggle.contains(e.target)
+  ) {
+    sidebar.classList.remove("open");
+    layout.classList.remove("sidebar-open");
+  }
+});
+
+  const chips = document.querySelectorAll(".chip");
+  const eventCards = document.querySelectorAll(".event-card");
+
+  chips.forEach(chip => {
+    chip.addEventListener("click", () => {
+      // remove active from all chips
+      chips.forEach(c => c.classList.remove("active"));
+      chip.classList.add("active");
+
+      const selectedCategory = chip.dataset.category;
+
+      eventCards.forEach(card => {
+        const cardCategory = card.dataset.category;
+
+        if (selectedCategory === "all" || cardCategory === selectedCategory) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  });
+
 loadEvents();
 
 function renderEvent(event) {
